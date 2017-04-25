a61acd947f92dc7bdb7224d0723e80f9e728262b
daton <rapidclimate@gmail.com>
2017-04-24 22:29:02 -0500

    asasas

--- app/src/main/java/sierra/org/sierraguadalupe/MainActivity.java
+++ app/src/main/java/sierra/org/sierraguadalupe/MainActivity.java
@@ -5,27 +5,46 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
+    LinearLayout primero;
+    LinearLayout segundo;
+    LinearLayout tercero;
+    FrameLayout content;
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
+
+
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_home: {
+
+                       content.removeAllViews();
+                    content.addView(primero);
+
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                }
+                case R.id.navigation_dashboard: {
+                    content.removeAllViews();
+                    content.addView(segundo);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                }
+                case R.id.navigation_notifications: {
+                    content.removeAllViews();
+                    content.addView(tercero);
                     return true;
+                }
             }
             return false;
         }
@@ -37,7 +56,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        primero= (LinearLayout) MainActivity.super.findViewById(R.id.primero);
+       segundo= (LinearLayout) MainActivity.super.findViewById(R.id.segundo);
+       tercero= (LinearLayout) MainActivity.super.findViewById(R.id.tercero);
+       content= (FrameLayout) MainActivity.super.findViewById(R.id.content);
+
+       content.removeAllViews();
+        content.addView(primero);
+
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
